8764dfdbc06ef54d66f22e3334a6331f967e618c
pengfuyuzhen <pfyz2011@gmail.com>
2018-03-26 15:07:37 -0400

    use delay

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -3,8 +3,6 @@ const int yPort = A1;
 const int fsrPort = A5;
 const int resetPort = 13;
 
-unsigned long targetTime = 0;
-const unsigned long interval = 10; //TODO: How fast should we read
 void setup(){
   Serial.begin(115200);
 }
@@ -14,12 +12,10 @@ void loop(){
   if (digitalRead(resetPort) == 0) {
     Serial.println("rst");
   } else if (analogRead(fsrPort) < 1023) {
-    if(millis() >= targetTime){
-      targetTime= millis()+interval;
-      String x = String(analogRead(xPort));
-      String y = String(analogRead(yPort));
-      String data = x + "," + y;
-      Serial.println(data);
-    }
+    String x = String(analogRead(xPort));
+    String y = String(analogRead(yPort));
+    String data = x + "," + y;
+    Serial.println(data);
+    delay(10);
   }
 }
